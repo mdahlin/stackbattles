d75d45abced0d7b15312c01b33e512de17dc0ecb
Michael Dahlin <michael.r.dahlin@gmail.com>
2021-10-28 23:22:31 -0500

    adds functionality for leaderboard

--- cards_msg_discord.py
+++ cards_msg_discord.py
@@ -8,7 +8,9 @@ from discordapi import DiscordAPI
 man = CardManager(player_puuid=SQUAD_PUUID["Dahlin"])
 discord_api = DiscordAPI(TOKEN)
 # initialize
+CHANNEL_ID = '894438526580555817'
 last_match_id = man.getCards(5)[0]
+last_message_id = discord_api.getChannelMessages(CHANNEL_ID, {"limit": 1})[0]["id"]
 
 def getCardsString(cards):
     cards.sort(key=lambda x: x[3], reverse=True)
@@ -19,6 +21,15 @@ def getCardsString(cards):
         string += '\n'
     return string
 
+def checkDiscordMessages(messages_json: list, keyword: str) -> bool:
+    """checks if messages includes keyword"""
+    messages = [msg["content"] for msg in messages_json]  # parse json
+
+    if any(keyword in msg for msg in messages):
+        return True
+
+    return False
+
 while True:
     try:
         print("Checking for new matches")
@@ -27,10 +38,21 @@ while True:
         if match_id != last_match_id:
             print("New match found")
             # occasionally request errors, so just keep trying
-            discord_api.sendMessage('894438526580555817', 
-                    {"content": getCardsString(cards)})
+            discord_api.sendMessage(CHANNEL_ID, {"content": getCardsString(cards)})
 
             last_match_id = match_id
+
+        print("Checking for keyword")
+        discord_messages = discord_api.getChannelMessages(CHANNEL_ID,
+                                                          {"after": last_message_id})
+
+        if checkDiscordMessages(discord_messages, "!leaderboard"):
+            print("Keyword match found")
+            discord_api.sendMessage(CHANNEL_ID, {"content": "on you foof"})
+
+        if discord_messages:  # if list is not empty, update last  message
+            last_message_id = discord_messages[0]["id"]  # first element is latest
+
     except Exception as e:
         print(e)
         pass
--- discordapi.py
+++ discordapi.py
@@ -1,4 +1,3 @@
-# NOT BEING USED UNTIL `discord.py` STOPS WORKING
 import requests
 
 class DiscordAPI:
@@ -8,7 +7,29 @@ class DiscordAPI:
     def __init__(self, TOKEN: str):
         self.headers={'Authorization': f'Bot {TOKEN}'}
 
+    def _expandPathToUrl(self, path: str, params: dict = {}) -> str:
+        """adds onto the `path` with additional query `params`"""
+        url = path
+        url += '?' if params else ''
+        return url + '&'.join([key + '=' + str(params[key]) for key in params])
+
     def sendMessage(self, channel_id: str, body: dict) -> None:
         requests.post(self.endpoint + f'/channels/{channel_id}/messages',
                       data=body, headers=self.headers)
         return None
+
+    def getChannelMessages(self, channel_id: str, params: dict = {}) -> list:
+        """valid params are `around`, `before`, `after`, `limit`"""
+        base_path = self.endpoint + f'/channels/{channel_id}/messages'
+        path = self._expandPathToUrl(base_path, params)
+        res = requests.get(path, headers=self.headers)
+        return res.json()
+
+if __name__ == "__main__":
+    from secrets import TOKEN
+    api = DiscordAPI(TOKEN)
+    channel_id = '894438526580555817'
+
+    res = api.getChannelMessages(channel_id, {"limit": 5})
+    print(res)
+
